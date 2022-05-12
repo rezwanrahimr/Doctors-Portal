a0f0d8ba06c9434b2836bfc2b046cfa2cf057654
rezwanrahimr <rezwanrahim69@gmail.com>
2022-05-12 17:36:18 +0600

    set home page info

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,15 +2,16 @@ import React from 'react';
 import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
+    
     return (
 
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="hero min-h-screen px-12 bg-local" >
+             <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
+                    <div className='text-left'>
                         <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                         <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary">Get Started</button>
                     </div>
                 </div>
             </div>
---
+++ src/components/Info/Info.js
@@ -0,0 +1,59 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import'./info.css';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
+const Info = () => {
+    return (
+        <div className=''>
+            <div className="flex w-full px-12 text-white">
+                <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center bg-gradient-to-r from-primary to-secondary">
+                    <div className='flex'>
+                    <div className='text-6xl text-white'>
+                    <FontAwesomeIcon icon={faClock} />
+
+                    </div>
+                    <div className='pl-2 text-left'>
+                        <h1>Opening Hours</h1>
+                        <small>Lorem ipsum dolor sit amet.</small>
+                    </div>
+
+                    </div>
+                   
+                </div>
+                <div className="px-2"></div>
+                <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center bg-accent">
+                    <div className='flex'>
+                    <div className='text-6xl text-white'>
+                    <FontAwesomeIcon icon={faLocationDot} />
+
+                    </div>
+                    <div className='pl-2 text-left'>
+                        <h1>Visit our location</h1>
+                        <small>Brooklyn, NY 10036, United States</small>
+                    </div>
+
+                    </div>
+                   
+                </div>
+                <div className="px-2"></div>
+                <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center bg-gradient-to-r from-primary to-secondary">
+                    <div className='flex'>
+                    <div className='text-6xl text-white'>
+                    <FontAwesomeIcon icon={faPhone} />
+
+                    </div>
+                    <div className='pl-2 text-left'>
+                        <h1>Contact us now</h1>
+                        <small>+000 123 456789</small>
+                    </div>
+
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
