1107aec54f0724a6a99cbd5440e8e63ef0ba00b0
rezwanrahimr <rezwanrahim69@gmail.com>
2022-05-12 10:53:35 +0600

    route set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header'
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Appointment from './components/Appointment/Appointment';
+import Reviews from './components/Reviews/Reviews';
+import ContactUs from './components/ContactUs/ContactUs';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/home' element={<Home></Home>}></Route>
+       <Route path='/about' element={<About></About>}></Route>
+       <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+       <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+       <Route path='/contactus' element={<ContactUs></ContactUs>}></Route>
+       <Route path='/login' element={<Login></Login>}></Route>
+     </Routes>
     </div>
   );
 }
